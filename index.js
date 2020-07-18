88396d4f6ea880dfc8a867b5fd96317d0bc6515a
Arkesh Jaiswal <aj.xddx@gmail.com>
2020-07-18 23:51:17 +0530

    task2: post route on / to return a list of lists

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const Search = require('./search');
+const fetch = require('node-fetch');
 
 app.use(express.json());
 
@@ -13,6 +14,50 @@ app.get('/', (req, res) => {
   const search = Search();
   res.header("Content-Type",'application/json');
   res.send(JSON.stringify(search.autocomplete('your problem', 4), null, 4));
+});
+
+app.post('/', async (req, res) => {
+  let list = [];
+  let books = [];
+  for(query of req.body.queries) {
+    const search = Search();
+    let results = search.autocomplete(query, req.body.k);
+    results.forEach(reco => {
+      delete reco.count;
+      delete reco.index;
+      delete reco.prefer;
+      reco.query = query;
+      books.push(reco.id); // create an array of book_ids; use this to generate an array of promises
+    });
+    list.push(results);
+  }
+  // console.log(books);
+  
+  const url = 'https://ie4djxzt8j.execute-api.eu-west-1.amazonaws.com/coding';
+  let authors = await Promise.all( // use Promise.all to resolve the array of promises as one promise
+    books.map(async id => {
+      let res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({"book_id": id})
+      });
+      data = await res.json();
+      return data.author // create an array of authors from book_ids
+    })
+  );
+  // console.log(authors);
+
+  // Put back author key into results
+  let i=0;
+  list = list.map(query => query.map(result => { // double map because result is a list of lists
+    i++;
+    return {
+      ...result,
+      author: authors[i-1]
+    }
+  }))
+
+  res.header("Content-Type",'application/json');
+  res.json(list);
 });
 
 app.listen(8000, () =>
