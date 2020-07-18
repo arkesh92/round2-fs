fb69eaf6e46fb887cf95ce7113cf4932cb260568
Arkesh Jaiswal <aj.xddx@gmail.com>
2020-07-18 22:45:43 +0530

    task1: read and display data

---
+++ search.js
@@ -0,0 +1,9 @@
+const Search = () => {
+  // declaring class variables
+  let results = []; // array to store autocomplete suggestions
+  let visited = {}; // hashmap to store summaries added to autocomplete
+}
+
+// non-function property initialized outside the class
+Search.data = require('./data');
+console.log(Search.data);
